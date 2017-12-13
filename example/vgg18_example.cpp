50b3f09c08bfb42845f9038d9782b655f1ab665e
okdshin <kokuzen@gmail.com>
2017-12-14 00:52:27 +0900

    fix little

--- example/vgg18_example.cpp
+++ example/vgg18_example.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+
 #include <opencv2/opencv.hpp>
 
 #include <instant/array.hpp>
@@ -23,14 +24,14 @@ auto crop_and_resize(cv::Mat mat, cv::Size const& size) {
 auto reorder_to_nchw(cv::Mat const& mat) {
     assert(mat.channels() == 3);
     std::unique_ptr<float[]> data(
-        new float[mat.channels() * mat.rows * mat.cols]);
-    for (int y = 0; y < mat.rows; ++y) {
-        for (int x = 0; x < mat.cols; ++x) {
+      new float[mat.channels() * mat.rows * mat.cols]);
+    for(int y = 0; y < mat.rows; ++y) {
+        for(int x = 0; x < mat.cols; ++x) {
             // cv::imread loads image BGR order so reordering here to RGB
-            for (int c = mat.channels() - 1; c >= 0; --c) {
+            for(int c = mat.channels() - 1; c >= 0; --c) {
                 *(data.get() + (c * (mat.rows * mat.cols) + y * mat.cols + x)) =
-                    static_cast<float>(
-                        mat.data[y * mat.step + x * mat.elemSize() + c]);
+                  static_cast<float>(
+                    mat.data[y * mat.step + x * mat.elemSize() + c]);
             }
         }
     }
@@ -38,17 +39,18 @@ auto reorder_to_nchw(cv::Mat const& mat) {
 }
 
 template <typename InIter>
-auto extract_top_k_index_list(InIter first, InIter last,
-                 typename std::iterator_traits<InIter>::difference_type k) {
+auto extract_top_k_index_list(
+  InIter first, InIter last,
+  typename std::iterator_traits<InIter>::difference_type k) {
     using diff_t = typename std::iterator_traits<InIter>::difference_type;
     std::priority_queue<
-        std::pair<typename std::iterator_traits<InIter>::value_type, diff_t>>
-        q;
-    for (diff_t i = 0; first != last; ++first, ++i) {
+      std::pair<typename std::iterator_traits<InIter>::value_type, diff_t>>
+      q;
+    for(diff_t i = 0; first != last; ++first, ++i) {
         q.push({*first, i});
     }
     std::vector<diff_t> indices;
-    for (diff_t i = 0; i < k; ++i) {
+    for(diff_t i = 0; i < k; ++i) {
         indices.push_back(q.top().second);
         q.pop();
     }
@@ -58,7 +60,7 @@ auto extract_top_k_index_list(InIter first, InIter last,
 auto load_category_list(std::string const& synset_words_path) {
     std::ifstream ifs(synset_words_path);
     if(!ifs) {
-        throw std::runtime_error("File open error: "+synset_words_path);
+        throw std::runtime_error("File open error: " + synset_words_path);
     }
     std::vector<std::string> categories;
     std::string line;
@@ -77,35 +79,39 @@ int main(int argc, char** argv) {
     constexpr auto width = 224;
 
     cmdline::parser a;
-    a.add<std::string>("input_image", 'i', "input image path", false, "../data/Light_sussex_hen.jpg");
-    a.add<std::string>("model", 'm', "onnx model path", false, "../data/VGG16.onnx");
-    a.add<std::string>("synset_words", 's', "synset words path", false, "../data/synset_words.txt");
+    a.add<std::string>("input_image", 'i', "input image path", false,
+                       "../data/Light_sussex_hen.jpg");
+    a.add<std::string>("model", 'm', "onnx model path", false,
+                       "../data/VGG16.onnx");
+    a.add<std::string>("synset_words", 's', "synset words path", false,
+                       "../data/synset_words.txt");
     a.parse_check(argc, argv);
 
     auto input_image_path = a.get<std::string>("input_image");
     auto onnx_model_path = a.get<std::string>("model");
     auto synset_words_path = a.get<std::string>("synset_words");
 
-    cv::Mat image_mat = cv::imread(input_image_path.c_str(), CV_LOAD_IMAGE_COLOR);
+    cv::Mat image_mat =
+      cv::imread(input_image_path.c_str(), CV_LOAD_IMAGE_COLOR);
     if(!image_mat.data) {
-        throw std::runtime_error("Invalid input image path: "+input_image_path);
+        throw std::runtime_error("Invalid input image path: " +
+                                 input_image_path);
     }
     image_mat = crop_and_resize(std::move(image_mat), cv::Size(width, height));
     instant::array input_image(instant::dtype_t::float_,
                                {batch_size, channel_num, height, width},
                                reorder_to_nchw(image_mat));
-    assert(input_image.dtype() == instant::dtype_t::float_);
 
     auto onnx_model = instant::load_onnx(onnx_model_path);
 
     std::cout << "model input and output" << std::endl;
-    for (auto const& node : onnx_model.graph().node()) {
+    for(auto const& node : onnx_model.graph().node()) {
         std::cout << node.op_type() << "\t";
-        for (auto const& i : node.input()) {
+        for(auto const& i : node.input()) {
             std::cout << i << " ";
         }
         std::cout << "-> ";
-        for (auto const& i : node.output()) {
+        for(auto const& i : node.output()) {
             std::cout << i << " ";
         }
         std::cout << "\n";
@@ -113,29 +119,28 @@ int main(int argc, char** argv) {
 
     auto parameter_table = instant::make_parameter_table(onnx_model.graph());
     auto parameter_memory_table = instant::make_parameter_memory_table(
-        onnx_model.graph(), parameter_table, ::instant::get_context().engine());
+      onnx_model.graph(), parameter_table, ::instant::get_context().engine());
 
     auto conv1_1_in_name = "140326425860192";
     std::vector<std::tuple<std::string, instant::array, mkldnn::memory::format>>
-        input_list{
-            {conv1_1_in_name, input_image, mkldnn::memory::format::nchw}};
+      input_list{{conv1_1_in_name, input_image, mkldnn::memory::format::nchw}};
     auto variable_memory_table = instant::make_variable_memory_table(
-        input_list, ::instant::get_context().engine());
+      input_list, ::instant::get_context().engine());
 
     auto fc6_out_name = "140326200777976";
     auto softmax_out_name = "140326200803680";
     std::vector<std::string> required_output_name_list{fc6_out_name,
                                                        softmax_out_name};
     auto output_table = instant::run_model(
-        onnx_model.graph(), parameter_memory_table, variable_memory_table,
-        std::set<std::string>(required_output_name_list.begin(),
-                              required_output_name_list.end()));
-    for (auto const& required_output_name : required_output_name_list) {
+      onnx_model.graph(), parameter_memory_table, variable_memory_table,
+      std::set<std::string>(required_output_name_list.begin(),
+                            required_output_name_list.end()));
+    for(auto const& required_output_name : required_output_name_list) {
         std::cout << required_output_name << ": ";
-        for (int i = 0; i < 5; ++i) {
-            std::cout << *(static_cast<float*>(
-                               output_table[required_output_name].data()) +
-                           i);
+        for(int i = 0; i < 5; ++i) {
+            std::cout << *(
+              static_cast<float*>(output_table[required_output_name].data()) +
+              i);
         }
         std::cout << "...\n";
     }
@@ -144,8 +149,10 @@ int main(int argc, char** argv) {
 
     auto categories = load_category_list(synset_words_path);
     auto top_k = 5;
-    auto top_k_indices = extract_top_k_index_list(instant::fbegin(softmax_out), instant::fend(softmax_out), top_k);
+    auto top_k_indices = extract_top_k_index_list(
+      instant::fbegin(softmax_out), instant::fend(softmax_out), top_k);
     for(auto ki : top_k_indices) {
-        std::cout << ki << " " << instant::fat(softmax_out, ki) << " " << categories.at(ki) << std::endl;
+        std::cout << ki << " " << instant::fat(softmax_out, ki) << " "
+                  << categories.at(ki) << std::endl;
     }
 }
