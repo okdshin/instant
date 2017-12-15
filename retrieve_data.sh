3a84bbac1d878f171c5c034c590aa2d0bc7813ea
okdshin <kokuzen@gmail.com>
2017-12-15 14:49:24 +0900

    add retrieve_data.sh

---
+++ retrieve_data.sh
@@ -0,0 +1,3 @@
+wget https://www.dropbox.com/s/bjfn9kehukpbmcm/VGG16.onnx?dl=1 -O ./data/VGG16.onnx
+wget https://raw.githubusercontent.com/HoldenCaulfieldRye/caffe/master/data/ilsvrc12/synset_words.txt -O ./data/synset_words.txt
+wget https://upload.wikimedia.org/wikipedia/commons/5/54/Light_sussex_hen.jpg -O ./data/Light_sussex_hen.jpg
