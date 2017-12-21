58eb3331e90cfb7c9906ae8ee2d58297eba966d8
okdshin <kokuzen@gmail.com>
2017-12-21 14:28:29 +0900

    update travis

--- .travis/install_mkldnn.sh
+++ .travis/install_mkldnn.sh
@@ -1,9 +1,9 @@
-if [ ! -d "$HOME/mkl-dnn/build/src" ]; then
+if [ ! -d "$HOME/mkl-dnn/build/lib" ]; then
     git clone https://github.com/01org/mkl-dnn.git
     cd mkl-dnn/scripts && bash ./prepare_mkl.sh && cd ..
     sed -i 's/add_subdirectory(examples)//g' CMakeLists.txt
     sed -i 's/add_subdirectory(tests)//g' CMakeLists.txt
-    mkdir -p build && cd build && cmake .. && make
+    mkdir -p build && cd build && cmake -DCMAKE_INSTALL_PREFIX=$HOME/mkl-dnn .. && make
     sudo make install
     cd ..
 else
