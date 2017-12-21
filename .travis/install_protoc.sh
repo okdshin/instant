3dffecb7d94bdc5990abf965e6b3d669a07ee27f
okdshin <kokuzen@gmail.com>
2017-12-21 23:59:44 +0900

    add patch for protoc

--- .travis/install_protoc.sh
+++ .travis/install_protoc.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 if [ ! -d "$HOME/protoc/lib" ]; then
-    wget https://github.com/google/protobuf/archive/v3.5.1.tar.gz -O protobuf.tar.gz
+    wget https://github.com/google/protobuf/archive/v2.6.1.tar.gz -O protobuf.tar.gz
     tar -xzvf protobuf.tar.gz
-    mv protobuf-3.5.1 protobuf
+    mv protobuf-2.6.1 protobuf
     cd protobuf
+    patch -u autogen.sh < $HOME/instant/.travis/autogen.patch
     ./autogen.sh
     ./configure --prefix=$HOME/protoc
     make && make install
