bc9df9e484c98e40aeec05bd9bb0a555e317c5ab
okdshin <kokuzen@gmail.com>
2017-12-21 14:53:33 +0900

    update travis

--- .travis/install_protoc.sh
+++ .travis/install_protoc.sh
@@ -2,7 +2,11 @@
 if [ ! -d "$HOME/protoc/lib" ]; then
     wget https://github.com/google/protobuf/archive/v2.6.1.tar.gz -O protobuf-2.6.1.tar.gz
     tar -xzvf protobuf-2.6.1.tar.gz
-    cd protobuf-2.6.1 && ./configure --prefix=$HOME/protoc && make && make install
+    cd protobuf-2.6.1
+    ./autogen.sh
+    ./configure --prefix=$HOME/protoc
+    make && make install
+    cd ..
 else
     echo "Using cached directory."
 fi
