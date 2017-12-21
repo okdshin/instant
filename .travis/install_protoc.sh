071711a2bdc385e85f340f15c65a4b1f7c569f42
okdshin <kokuzen@gmail.com>
2017-12-21 14:43:21 +0900

    update travis

--- .travis/install_protoc.sh
+++ .travis/install_protoc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 if [ ! -d "$HOME/protoc/lib" ]; then
-    wget https://protobuf.googlecode.com/files/protobuf-2.6.1.tar.gz
+    wget https://github.com/google/protobuf/archive/v2.6.1.tar.gz
     tar -xzvf protobuf-2.6.1.tar.gz
     cd protobuf-2.6.1 && ./configure --prefix=$HOME/protoc && make && make install
 else
