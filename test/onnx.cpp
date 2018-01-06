9a46830aa2e0a6a84ed2e1c07f5fadd509ca3a6f
okdshin <kokuzen@gmail.com>
2018-01-06 11:35:37 +0900

    fix

--- test/onnx.cpp
+++ test/onnx.cpp
@@ -1,37 +1,40 @@
-#include <gtest/gtest.h>
-#include <onnx.pb.h>
 #include <fstream>
+#include <gtest/gtest.h>
 #include <instant/onnx.hpp>
 #include <iostream>
 #include <numeric>
+#include <onnx.pb.h>
 #include <string>
 
 #include <google/protobuf/io/coded_stream.h>
 #include <google/protobuf/io/zero_copy_stream_impl.h>
 
 namespace instant {
-namespace {
+    namespace {
 
-class ONNXTest : public ::testing::Test {};
+        class ONNXTest : public ::testing::Test {};
 
-TEST_F(ONNXTest, load_onnx_model) {
-    auto [graph, param_table] = instant::load_onnx("../data/VGG16.onnx", {"140326201105432", /*"140326200803456"*//*, "140326200803680"*/});
-    std::cout << "param table" << std::endl;
-    for(auto [name, arr] : param_table) {
-        std::cout << name << " ";
-       for(auto d : arr.dims()) {
-           std::cout << d << " ";
-       }
-       std::cout << "\n";
-    }
-    std::cout << "graph" << std::endl;
-    for(auto const& node_set : graph) {
-        for(auto const& node : node_set) {
-            std::cout << op_type_to_string(node.op_type()) << " ";
+        TEST_F(ONNXTest, load_onnx_model) {
+            auto[graph, param_table] = instant::load_onnx(
+              "../data/VGG16.onnx",
+              {"140326201105432",
+               /*"140326200803456"*/ /*, "140326200803680"*/});
+            std::cout << "param table" << std::endl;
+            for(auto[name, arr] : param_table) {
+                std::cout << name << " ";
+                for(auto d : arr.dims()) {
+                    std::cout << d << " ";
+                }
+                std::cout << "\n";
+            }
+            std::cout << "graph" << std::endl;
+            for(auto const& node_set : graph) {
+                for(auto const& node : node_set) {
+                    std::cout << op_type_to_string(node.op_type()) << " ";
+                }
+                std::cout << std::endl;
+            }
         }
-        std::cout << std::endl;
-    }
-}
 
-}  // namespace
-}  // namespace instant
+    } // namespace
+} // namespace instant
